5b6beceda36e2e7270d2ed663ec426cd8c5eb72b
Redennison <evan@dennisons.ca>
2021-10-06 19:52:03 -0400

    Update S3.py

--- CCC-2021/S3.py
+++ CCC-2021/S3.py
@@ -2,37 +2,35 @@ import sys
 
 def getTime(position):
     secs = 0
-    for i in range(0, N):
-        distance = abs(P[i] - position) - D[i]
+    for i in range(0, n):
+        distance = abs(p[i] - position) - d[i]
         if distance > 0:
-            secs += W[i] * distance
+            secs += w[i] * distance
     return secs
 
-P, W, D = [], [], []
-N, l, r = int(input()), sys.maxsize, -sys.maxsize - 1
+p, w, d = [], [], []
+n, l, r = int(input()), sys.maxsize, -sys.maxsize - 1
 
-for i in range(0, N):
+for i in range(0, n):
     values = input().split(" ")
-    P.append(int(values[0]))
-    W.append(int(values[1]))
-    D.append(int(values[2]))
+    p.append(int(values[0]))
+    w.append(int(values[1]))
+    d.append(int(values[2]))
 
-    if P[i] < l:
-        l = P[i]
-    if P[i] > r:
-        r = P[i]
+    l = min(l, p[i])
+    r = max(r, p[i])
 
 while (l <= r):
     mid = round(l + (r - l) / 2)
 
     time = getTime(mid) 
-    timePlusOne = getTime(mid+1)
+    timeplusOne = getTime(mid+1)
     timeMinusOne = getTime(mid-1)
 
-    if (time <= timePlusOne) and (time <= timeMinusOne):
+    if (time <= timeplusOne) and (time <= timeMinusOne):
         shortestTime = time 
         break 
-    elif time < timePlusOne:
+    elif time < timeplusOne:
         r = mid - 1
     else: 
         l = mid + 1
