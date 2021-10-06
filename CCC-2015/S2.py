03260f59a54a702b16ce5788b91c1515a284c8aa
Edennison <68529940+Redennison@users.noreply.github.com>
2021-10-05 21:17:44 -0700

    Update S2.py

--- CCC-2015/S2.py
+++ CCC-2015/S2.py
@@ -10,7 +10,6 @@ Solution:
     - Increment size to see if above sizes are available (if so, jersey available)
 '''
 
-
 from collections import Counter
 
 # GLOBAL VARIABLES
@@ -19,47 +18,32 @@ numAthletes = int(input())
 
 sizes = ('S','M','L')
 availableJerseySizes = Counter()
-requestedJerseys = list()
 numbersTaken = set()
-
 maxRequestsSatisfied = 0
-
-# FUNCTIONS
-def getSizeAvailable(requestedSize):
-    sizeNumber = sizes.index(requestedSize)
-    for i in range(sizeNumber, 3):
-        # Jersey available if size in sizes
-        if availableJerseySizes[sizes[i]] > 0:
-            return (True, sizes[i])
-    return (False)
-
 
 # Gets jerseys available as input
 for i in range(0, numJerseys):
     size = input()
     availableJerseySizes[size] += 1
-    print(i)
-print('hello')
-# Saves each players requested size + number as a tuple
-for i in range(0, numAthletes):
-    requests = input().split(" ")
-    requestedSize = requests[0]
-    requestedNumber = int(requests[1])
-    requestedJerseys[i] = (requestedSize, requestedNumber)
 
-for i in range(0, len(requestedJerseys)):
-    # Get requested size/number from index
-    requestedSize = requestedJerseys[i][0]
-    requestedNumber = requestedJerseys[i][1]
+for i in range(0, numAthletes):
+    requests = input().split(" ") 
+    requestedSize = requests[0] 
+    requestedNumber = int(requests[1]) 
 
     # Check if number isn't taken
     if requestedNumber not in numbersTaken:
 
-        sizeAvailable = getSizeAvailable(requestedSize)
-        if sizeAvailable[0]:
-            maxRequestsSatisfied += 1
-            # Sizes available is 1 less since 1 is now taken
-            availableJerseySizes[sizeAvailable[1]] -= 1
+        sizeNumber = sizes.index(requestedSize)
+        for i in range(sizeNumber, 3):
+            # Jersey available if size in sizes
+            if availableJerseySizes[sizes[i]] > 0:
+                maxRequestsSatisfied += 1
+                # Sizes available is 1 less since 1 is now taken
+                availableJerseySizes[sizes[i]] -= 1
+                # Number can no longer be taken
+                numbersTaken.add(requestedNumber)
+                break
 
 print(maxRequestsSatisfied)
 
