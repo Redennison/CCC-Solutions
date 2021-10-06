ec10dd3dc390ee81afa02f86c0962d26d40b974e
Redennison <evan@dennisons.ca>
2021-10-06 15:13:48 -0400

    Update S2.py

--- CCC-2015/S2.py
+++ CCC-2015/S2.py
@@ -1,48 +1,35 @@
-'''
-Points:
-- If don't have size, take closest size larger than requested (leaves larger sizes for larger requests)
-- Each player must have different number (even if different size jerseys)
-
-Solution:
-- Loop through each requested size/number
-    - If requested number in numbersTaken, jersey not available
-        - If size exists, jersey available
-        - If size doesn't exist, check the closest larger size available
-        - If exact or larger size available jersey available
-            - # can no longer be taken
-            - 1 less size of the jersey size taken
-            - Another athlete gets jersey they wanted
-'''
-
-# GLOBAL VARIABLES
+# Global variables
 numJerseys = int(input())
 numAthletes = int(input())
-availableJerseySizes = {'S': 0, 'M': 0, 'L': 0}
+jerseys = {}
+players = []
+sizes = ('S','M','L')
 numbersTaken = set()
 maxRequestsSatisfied = 0
 
-# Gets jerseys available as input
-for i in range(0, numJerseys):
-    availableJerseySizes[input()] += 1
+# Gets jerseys 
+for i in range(1, numJerseys+1):
+    jerseys[i] = input()
 
+# Get player requests
 for i in range(0, numAthletes):
-    requests = input().split(" ") 
-    requestedSize = requests[0] 
-    requestedNumber = int(requests[1]) 
+    request = input().split(" ")
+    size = request[0]
+    number = int(request[1])
+    players.append((size, number))
 
-    # Check if number isn't taken
-    if requestedNumber not in numbersTaken:
-
-        sizeIndex = list(availableJerseySizes.keys()).index(requestedSize)
+# Loops through players to determin in jersey available
+for player in players:
+    playersSize = player[0]
+    playersNumber = player[1]
+    # Ensure number hasn't been taken
+    if playersNumber not in numbersTaken:
+        sizeIndex = sizes.index(playersSize)
         for i in range(sizeIndex, 3):
-            size = list(availableJerseySizes.keys())[i]
-            # Jersey available if there is a jersey in that size
-            if availableJerseySizes[size] > 0:
+            if jerseys[playersNumber] == sizes[i]:
                 maxRequestsSatisfied += 1
-                # 1 less available jersey in chosen size
-                availableJerseySizes[size] -= 1
-                # Number can no longer be taken
-                numbersTaken.add(requestedNumber)
+                del jerseys[playersNumber]
+                numbersTaken.add(playersNumber)
                 break
 
 print(maxRequestsSatisfied)
